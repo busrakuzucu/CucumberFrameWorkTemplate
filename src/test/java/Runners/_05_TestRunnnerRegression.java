eb901393e3c9df72c8c5a384e84b68de0a421108
Busra <mdu9506@gmail.com>
2023-05-03 19:50:49 +0300

    JEnkins profile ekleme

--- src/test/java/Runners/_05_TestRunnnerRegression.java
+++ src/test/java/Runners/_05_TestRunnnerRegression.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
         // or yerine and yazılırsa hem smoke hem de regression tag i olanları çalıştırıyor
         // tags = "@SmokeTest and @Regression",//burada ise sadece iki etiketli olan featureslar çalışır.
         features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        plugin= {"json:target/cucumber/cucumber.json"}
 
 
 )
